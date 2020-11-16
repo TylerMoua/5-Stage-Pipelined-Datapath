2f363129d15946b6af5fac07b99c3d8010441a11
TylerMoua <41130020+TylerMoua@users.noreply.github.com>
2020-11-15 17:51:56 -0800

    Update MainALU.v
    Changed to 32-bit output

--- source/MainALU.v
+++ source/MainALU.v
@@ -1,7 +1,10 @@
 module MainALU(input [15:0]A, B, 
 		   input [2:0] ALUControl,
-		   output reg [15:0] Result,
+		   output reg [31:0] Result,
 		   output reg Zero, Negative, Overflow);
+
+wire [15:0] Result1, Result2;
+
 always @(*)
 begin
 
@@ -9,28 +12,32 @@ begin
 	Negative = 1'b0;
 	Zero = 1'b0;
 	Overflow = 1'b0;
-	
+	Result2= 0;
 	case (ALUControl)
 		//ADD
-		3'b000: Result = A + B;
+		3'b000: Result1 = A + B;
 		//SUB
-		3'b001: Result = A - B;
+		3'b001: Result1 = A - B;
 		//MOVE
-		3'b010: Result = B;
-		//SWAP --NEED TO FIX
-		3'b011: Result = A;
+		3'b010: Result1 = B;
+		//SWAP
+		3'b011: 
+		begin
+			Result1 = B;
+			Result2 = A;
+		end
 		//AND
-		3'b100: Result = A & B;
+		3'b100: Result1 = A & B;
 		//OR - 101, 110, or 111
-		3'b101: Result = A | B;
-		default: Result = A | B;
+		3'b101: Result1 = A | B;
+		default: Result1 = A | B;
 
 	endcase
 	
 	//Flag Assignemnts:
-	if(Result < 3'b000)
+	if(Result1 < 3'b000)
 		Negative = 1'b1;
-	if (Result == 3'b000)
+	if (Result1 == 3'b000)
 		Zero = 1'b1;
 		
 	/*
@@ -40,9 +47,12 @@ begin
 	OR
 	we have a positive result with two negative values (Right)
 	Ex. ~(0) & 1 & 1  = 1
+	Note that we are testing result1
 	*/
-	if ((Result[15]&(~A[15])&(~B[15])) | (~Result[15]&A[15]&B[15]))
+	if ((Result1[15]&(~A[15])&(~B[15])) | (~Result1[15]&A[15]&B[15]))
 		Overflow = 1'b1;
 
+	//Final assignment:
+	Result={Result2, Result1};
 end
 endmodule
