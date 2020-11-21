2cff6d0244a0e974cb9b27bfa06a133ab2e42d7a
TylerMoua <41130020+TylerMoua@users.noreply.github.com>
2020-11-21 13:46:27 -0800

    Completed Branch Equator Code

--- source/BranchEquator.v
+++ source/BranchEquator.v
@@ -1,11 +1,9 @@
 module BranchEquator(input [15:0] A, B, 
 					 input [2:0]  BranchSelect,
 					 input Branch, Jump,
-					 wire  [15:0] Result,
-					 wire Negative, Zero, BranchResult;
 					 output reg BranchingSoFlush);
 
-wire [15:0] Result1, Result2;
+reg Negative, Zero;
 
 always @(*)
 begin
@@ -13,21 +11,19 @@ begin
 	//Default Values:
 	Negative = 1'b0;
 	Zero = 1'b0;
-	Result = A - B;
 	
 	//Test Result for flags:
-	if(Result1 < 3'b000)
+	if(A < B)
 		Negative = 1'b1;
-	if (Result1 == 3'b000)
+	if (A == B)
 		Zero = 1'b1;
 	
 	//Test Flags for BranchResult
 	case(BranchSelect)
-	begin
 		//BLT
 		2'b00: BranchingSoFlush = (Negative & Branch) | Jump;
 		//BGT
-		2'b01: BranchingSoFlush = (!Negative & Branch) | Jump;
+		2'b01: BranchingSoFlush = (!Negative & !Zero & Branch) | Jump;
 		//BEQ, covers 2'b10, 2'b11
 		default: BranchingSoFlush = (Zero & Branch) | Jump;
 	endcase
--- source/BranchEquator_Fixture.v
+++ source/BranchEquator_Fixture.v
@@ -10,60 +10,60 @@ wire Overflow, BranchingSoFlush;
 initial
 begin
 	$display("\nResults:");
-	$monitor("A= %b B= %b BranchingsoFlush = %b\n", A, B, BranchingSoFlush);
+	$monitor("A= %b B= %b BranchingSoFlush = %b\n", A, B, BranchingSoFlush);
 end
-ALU alu(A, B, CTRL, R);
+BranchEquator BE(.A(A), .B(B), .BranchSelect(BranchSelect), .Branch(Branch), .Jump(Jump), .BranchingSoFlush(BranchingSoFlush));
 
 initial
 begin
-	Jump = 1b'0;
+	Jump = 1'b0;
 	Branch = 1'b1;
 	$display("\nBLT:");
 	BranchSelect = 2'b00;
-	A = 20;
-	B = 15;
+	A = 1;
+	B = 0;
 	#10;
-	A = 10;
-	B = 10;
+	A = 1;
+	B = 1;
 	#10;
-	A = 15;
-	B = 20;
+	A = 0;
+	B = 1;
 	#10;
 	$display("\nBGT:");
 	BranchSelect = 2'b01;
-	A = 20;
-	B = 15;
+	A = 1;
+	B = 0;
 	#10;
-	A = 10;
-	B = 10;
+	A = 1;
+	B = 1;
 	#10;
-	A = 15;
-	B = 20;
+	A = 0;
+	B = 1;
 	#10;
 	$display("\nBEQ:");
 	BranchSelect = 2'b10;
-	A = 20;
-	B = 15;
+	A = 1;
+	B = 0;
 	#10;
-	A = 10;
-	B = 10;
+	A = 1;
+	B = 1;
 	#10;
-	A = 15;
-	B = 20;
+	A = 0;
+	B = 1;
 	#10;
 	$display("\nJump:");
 	Jump = 1'b1;
 	BranchSelect = 2'b00;
-	A = 20;
-	B = 15;
+	A = 1;
+	B = 0;
 	#10;
 	BranchSelect = 2'b01;
-	A = 10;
-	B = 10;
+	A = 1;
+	B = 1;
 	#10;
-	BranchSelect = 2'b10;
-	A = 15;
-	B = 20;
+	BranchSelect = 2'b11;
+	A = 0;
+	B = 1;
 	#10;
 	
 end
