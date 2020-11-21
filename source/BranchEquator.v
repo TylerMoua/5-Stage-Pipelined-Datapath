9f36ded8c64178d62c8ce9be0ff7b750434719d9
TylerMoua <41130020+TylerMoua@users.noreply.github.com>
2020-11-21 13:07:45 -0800

    New Module for BranchEquator Component

--- source/BranchEquator.v
+++ source/BranchEquator.v
@@ -1,13 +1,18 @@
-module MainALU(input [15:0]A, B, 
-		   input [2:0] ALUControl,
-		   output reg [31:0] Result);
+module BranchEquator(input [15:0]A, B, 
+		   input [2:0] BranchSelect,
+		   output reg [31:0] BranchResult);
 
 wire [15:0] Result1, Result2;
 
 always @(*)
 begin
 
-	case (ALUControl)
+	//Default Values:
+	Negative = 1'b0;
+	Zero = 1'b0;
+	Overflow = 1'b0;
+	Result2= 0;
+	case (BranchSelect)
 		//ADD
 		3'b000: Result1 = A + B;
 		//SUB
@@ -28,6 +33,7 @@ begin
 
 	endcase
 	
-	Result={Result2, Result1};
+	//Final assignment:
+	BranchResult={Result2, Result1};
 end
 endmodule
