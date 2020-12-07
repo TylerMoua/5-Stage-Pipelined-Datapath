dd396945df73a6773065537ab181282098dd225b
TylerMoua <41130020+TylerMoua@users.noreply.github.com>
2020-12-06 20:59:33 -0800

    Changed how instructions are read

--- source/InstructionMemory.v
+++ source/InstructionMemory.v
@@ -3,10 +3,9 @@ module InstructionMemory #(parameter N = 16)
 						   input clk, rst,
 						   output [15:0] Instruction);
 
-//NEED: reg [7:0] Instructions [100:0] where Instruction[0] and Instruction[1] = One instruction
-reg [15:0] Instructions [N-1:0];
+reg [7:0] Instructions [N-1:0];
 
-	assign Instruction = Instructions [ReadAddress];
+	assign Instruction = {Instructions[ReadAddress],Instructions[ReadAddress+1]};
 
 always @(posedge clk, negedge rst)
 begin
@@ -20,13 +19,17 @@ begin
 	//*
 	//Test Values for Hazards:
 		//ADD R0 R1: R0 = 0 + 1 = 1
-		Instructions[0]<=16'h1010;
+		Instructions[0]<=8'h10;
+		Instructions[1]<=8'h10;
 		//LW R0 0(R2): R4 = data in mem[2]
-		Instructions[2]<=16'h6002;		
+		Instructions[2]<=8'h60;	
+		Instructions[3]<=8'h02;		
 		//BGT op0 04 - > True : pc = 6+8= e, False: pc = 4
-		Instructions[4]<=16'hD004;
+		Instructions[4]<=8'hD0;
+		Instructions[5]<=8'h04;
 		//SUB R0 R1: R0 = 3 - 1 = 3
-		Instructions[6]<=16'h1011;
+		Instructions[6]<=8'h10;
+		Instructions[7]<=8'h11;
 	//*/
 	
 	
